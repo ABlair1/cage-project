91b4ce7414414eeaaca563004ff7226a3de2040d
Andrew <andrewjblair@gmail.com>
2021-09-01 19:29:07 -0400

    Updating filmography model, adding imdbpy test file

--- nic_uncaged/uncaged_app/models.py
+++ nic_uncaged/uncaged_app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 class Filmography(models.Model):
+    imdb_id = models.CharField(max_length=20)
     title = models.CharField(max_length=100)
     year = models.SmallIntegerField()
     role = models.CharField(max_length=100)
     likes = models.IntegerField(default=0)
     # page_link = models.URLField()
-    # rating = models.CharField(max_length=8)
-    # runtime = 
-    # synopsis = 
+    # imdb_rating = models.CharField(max_length=8)
+    # uncaged_rating = models.CharField(max_length=8)
+    runtime = models.CharField(max_length=20)
+    plot = models.TextField()
