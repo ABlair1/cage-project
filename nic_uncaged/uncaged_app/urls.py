f17a25337885f6d076f6332a1da70c1847012d5e
Andrew <andrewjblair@gmail.com>
2021-09-13 22:31:21 -0400

    Adding catalog page and movie pages for each movie in catalog

--- nic_uncaged/uncaged_app/urls.py
+++ nic_uncaged/uncaged_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.catalog, name='catalog'),
+    path('entry/<int:pk>', views.CatalogEntryView.as_view(), name='catalog-entry'),
 ]
--- nic_uncaged/uncaged_app/views.py
+++ nic_uncaged/uncaged_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 from .models import Filmography
 
-def index(request):
+def catalog(request):
     # Get all films and film count from Nic Cage Filmography
     films = Filmography.objects.all()
     film_count = Filmography.objects.all().count()
@@ -12,5 +13,8 @@ def index(request):
         'film_count': film_count,
     }
 
-    # Render the home template with the data in the context variable
-    return render(request, 'home.html', context=context)
+    # Render the movie catalog template with the data in the context variable
+    return render(request, 'catalog.html', context=context)
+
+class CatalogEntryView(generic.DetailView):
+    model = Filmography
